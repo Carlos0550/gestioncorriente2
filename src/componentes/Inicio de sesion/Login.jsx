5255c875b421a3c90e57dd0720853ff636950d8f
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-09 18:15:47 -0300

    Componente actualizado
    Se agregó manejo de errores y de estado para el usuario

--- src/componentes/Inicio de sesion/Login.jsx
+++ src/componentes/Inicio de sesion/Login.jsx
@@ -1,15 +1,32 @@
-import { SignInButton } from '@clerk/clerk-react'
-import React from 'react'
+import { SignInButton, useUser } from '@clerk/clerk-react'
+import React, { useEffect, useState } from 'react'
 import "./login.css"
+import { useAuth } from "@clerk/clerk-react";
+import { useNavigate } from 'react-router-dom';
+import { useAppContext } from '../../context/AppContext';
+import { message, notification } from 'antd';
 
 function Login() {
-    
+  const navigate = useNavigate();
+  const { administrator,authorized, listaUsuarios, nonAuthorized } = useAppContext()
+  const { isSignedIn } = useAuth()
+  const { isLoaded } = useAuth()
+
+   
+  useEffect(()=>{
+      if (isSignedIn && authorized && administrator) {
+          navigate("/dashboard")
+      }else if(isSignedIn && authorized && !administrator){
+          navigate("/dashboard")
+      }
+      console.log(isSignedIn)
+  },[isSignedIn, administrator, authorized, navigate])
   return (
     <div className='container-wrapper'>
         <div className="login__wrapper">
             <div className="login__form">
                 <h1 className='title'>Bienvenido a Gestión Corriente</h1>
-                <SignInButton className="login__button" />
+                {nonAuthorized ? <p>Redirigiendote en 3...</p> : !isLoaded ? "Aguarde..." : <SignInButton className="login__button" />}
             </div>
         </div>
     </div>
