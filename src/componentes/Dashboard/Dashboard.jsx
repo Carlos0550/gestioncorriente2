0c43004ab3096edf3ed73e6ee1ee77ff0fd9a303
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-10 16:30:07 -0300

    Funciones para conceder o denegar acceso al sistema

--- src/componentes/Dashboard/Dashboard.jsx
+++ src/componentes/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Row,Col, Table, Card, Popconfirm } from "antd"
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Table, Card, Popconfirm } from "antd"
 import "./dashboard.css"
 import Navbar from '../Navbar/Navbar'
 import { totalAccountsToPay } from '../Estructuras de ejemplo/TotalCuentasCobrar'
@@ -12,23 +12,29 @@ import { DeleteOutlined } from "@ant-design/icons";
 function Dashboard() {
 
     const navigate = useNavigate()
-    const { listaUsuarios, deleteUser } = useAppContext()
-    
+    const { listaUsuarios, deleteUser, grantOrDenyAccess, currentUser } = useAppContext()
+
     const orderedUsers = listaUsuarios
-    .sort((a,b)=> a.id - b.id)
+        .sort((a, b) => a.id - b.id)
     const [deletingUser, setDeletingUser] = useState(false)
-    const handleDeleteUser = async(id) => {
+    const handleDeleteUser = async (id) => {
         setDeletingUser(true)
         await deleteUser(id)
         setDeletingUser(false)
     }
- 
+
+    useEffect(()=>{
+        if (currentUser && !currentUser.administrador) {
+            navigate("/clientes")
+        }
+    },[navigate])
+
     const usersStructureTable = [
-        
+
         {
             key: 1,
             title: "Usuario",
-            render: (_,record) => (
+            render: (_, record) => (
                 <p>{record.username}</p>
             )
         },
@@ -66,62 +72,61 @@ function Dashboard() {
             render: (_, record) => (
                 <picture className="user__image-container"><img src={record.userimage} alt="" /></picture>
             )
-        },{
+        }, {
             key: 4,
             title: "Acciones",
             render: (_, record) => (
                 <>
-                    <Space direction='vertical'>
-                        <Space >
-                            {record.administrador ? "Quitar Administrador" : "Hacer administrador"} <Switch checked={record.administrador}/>
+                    {record.administrador === true ? null : (
+                        <Space direction='vertical'>
+                            <Space>
+                                {record.autorizado ? "Quitar Acceso" : "Conceder Acceso"} <Switch checked={record.autorizado} onChange={() => grantOrDenyAccess(record.id)} />
+                            </Space>
+                            <Popconfirm
+                                title="¿Estás seguro de eliminar este usuario?"
+                                description="Si es administrador, perderá el acceso al sistema. asegurese de tener otro usuario como administrador."
+                                onConfirm={() => handleDeleteUser(record.id)}
+                                okText="Si, eliminar"
+                                cancelText="Cancelar"
+                                okButtonProps={{ loading: deletingUser }}
+                            >
+                                <Button type="primary" danger>Eliminar Usuario <DeleteOutlined /></Button>
+                            </Popconfirm>
                         </Space>
-                        <Space>
-                            {record.autorizado ? "Quitar Acceso" : "Conceder Acceso"} <Switch checked={record.autorizado}/>
-                        </Space>
-                        <Popconfirm
-                        title="¿Estás seguro de eliminar este usuario?"
-                        description="Si es administrador, perderá el acceso al sistema. asegurese de tener otro usuario como administrador."
-                        onConfirm={() => handleDeleteUser(record.id)}
-                        okText="Si, eliminar"
-                        cancelText="Cancelar"
-                        okButtonProps={{ loading: deletingUser }}
-                        >
-                            <Button type="primary" danger>Eliminar Usuario <DeleteOutlined/></Button>
-                        </Popconfirm>
-                    </Space>
+                    )}
                 </>
             )
         }
     ]
-    
-  return (
-    <>
-        <Navbar/>        
-        <div className='container-wrapper'>
-            <h1 className='title'>Panel de Administración</h1>
-            <div className="dashboard__wrapper">
-            <Row gutter={[16, 16]}>
-                <Col sx={24} sm={24} md={12} lg={14}>
-                    <Card title="Usuarios con acceso">
-                        <Table columns={usersStructureTable} dataSource={orderedUsers} style={{minWidth: "100%"}} pagination={{pageSize: 5}} scroll={{x: 800}}/>
-                    </Card>
-                </Col>
-                <Col sx={24} md={12} lg={10}>
-                    <Card title="Pagos recientes">
-                        <Table columns={pagosRecientesEstructura}  style={{minWidth: "100%"}} pagination={{pageSize: 5}}/>
-                    </Card>
-                </Col>
-                <Col sx={24} sm={24} md={12} lg={14}>
-                    <Card title="Vencimientos del mes">
-                        <Table columns={totalAccountsToPay}  style={{minWidth: "100%"}} pagination={{pageSize: 4}}/>
-                    </Card>
-                </Col>
-                
-            </Row>
+
+    return (
+        <>
+            <Navbar />
+            <div className='container-wrapper'>
+                <h1 className='title'>Panel de Administración</h1>
+                <div className="dashboard__wrapper">
+                    <Row gutter={[16, 16]}>
+                        <Col sx={24} sm={24} md={24} lg={24}>
+                            <Card title="Usuarios con acceso">
+                                <Table columns={usersStructureTable} dataSource={orderedUsers} style={{ minWidth: "100%" }} pagination={{ pageSize: 5 }} scroll={{ x: 800 }} />
+                            </Card>
+                        </Col>
+                        <Col sx={24} sm={24} md={12} lg={12}>
+                            <Card title="Pagos recientes">
+                                <Table columns={pagosRecientesEstructura} scroll={{ x: 800 }} style={{ minWidth: "100%" }} pagination={{ pageSize: 5 }} />
+                            </Card>
+                        </Col>
+                        <Col sx={24} sm={24} md={12} lg={12}>
+                            <Card title="Vencimientos del mes">
+                                <Table columns={totalAccountsToPay} scroll={{ x: 800 }} style={{ minWidth: "100%" }} pagination={{ pageSize: 4 }} />
+                            </Card>
+                        </Col>
+
+                    </Row>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Dashboard
