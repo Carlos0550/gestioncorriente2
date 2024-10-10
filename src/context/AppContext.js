1bdb5e18b98d7c9ec936b0a7ca4f7305079b154d
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-10 16:30:41 -0300

    Se añadieron nuevas funciones para los roles, y clientes

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -33,11 +33,11 @@ export const AppContextProvider = ({ children }) => {
             userId: user.id || "",
             userImage: user.imageUrl || ""
         };
-        
+
         for (const key in data) {
             formData.append(key, data[key]);
         }
-               
+
         try {
             const response = await fetch(`http://localhost:4000/save-new-user`, {
                 method: "POST",
@@ -45,31 +45,33 @@ export const AppContextProvider = ({ children }) => {
             });
 
             if (!response.status === 200) {
-                const errorMessage = await response.text(); 
+                const errorMessage = await response.text();
                 throw new Error(`Error en la verificación del usuario: ${errorMessage}`);
             }
 
             const data = await response.json();
             if (data.administrador === true && data.autorizado === true) {
                 setAdministrator(true)
-                setAuthorized(true)  
-                setCurrentUser(data)              
+                setAuthorized(true)
+                setCurrentUser(data.currentUser)
+                await getAllClients()
                 api.success({
                     message: "Acceso autorizado",
                     description: "Se ha verificado el usuario correctamente",
                     placement: "topRight",
                     duration: 3
                 })
-            }else if(data.autorizado === true && data.administrador === false){
+            } else if (data.autorizado === true && data.administrador === false) {
                 setAuthorized(true)
-                setCurrentUser(data)              
+                setCurrentUser(data.currentUser)
+                await getAllClients()
                 notification.success({
                     message: "Acceso autorizado",
                     description: "Se ha verificado el usuario correctamente",
                     placement: "topRight",
                     duration: 3
                 })
-            }else{
+            } else {
                 setNonAuthorized(true)
                 notification.error({
                     message: "Acceso no autorizado",
@@ -82,36 +84,36 @@ export const AppContextProvider = ({ children }) => {
                 setTimeout(() => {
                     signOut()
                 }, 3000);
-                
+
             }
         } catch (error) {
             message.error(`Error al verificar el usuario: ${error.message}`, 3);
         }
     }
 
-    const getAllAllowUsers = async() => {
-        const hiddenMessage = message.loading("Obteniendo roles",0)
+    const getAllAllowUsers = async () => {
+        const hiddenMessage = message.loading("Obteniendo roles", 0)
         try {
             const response = await fetch(`${baseUrl.api}/get-all-users`)
             if (response.status === 200) {
                 const data = await response.json();
                 setListaUsuarios(data.usuarios)
-                
-            }else{
+
+            } else {
                 message.error("Error al obtener los roles")
             }
         } catch (error) {
             console.log(error)
             message.error("Error al obtener los roles")
-        }finally{
+        } finally {
             hiddenMessage()
         }
     }
-    
-    const deleteUser = async(id) => {
-        const hiddenMessage = message.loading("Eliminando usuario",0)
+
+    const deleteUser = async (id) => {
+        const hiddenMessage = message.loading("Eliminando usuario", 0)
         try {
-            const response = await fetch(`${baseUrl.api}/delete-user/${id}`,{
+            const response = await fetch(`${baseUrl.api}/delete-user/${id}`, {
                 method: "DELETE"
             })
             if (response.status === 200) {
@@ -123,14 +125,160 @@ export const AppContextProvider = ({ children }) => {
         } catch (error) {
             console.log(error)
             message.error("Error al eliminar el usuario")
+        } finally {
+            hiddenMessage()
+        }
+    }
+
+    const grantOrDenyAccess = async(id) => {
+        const hiddenMessage = message.loading("Guardando...", 0)
+        if (!id) {
+            message.error("No se encontro el usuario")
+            return;
+        }
+        try {
+            const response = await fetch(`${baseUrl.api}/grant-access/${id}`, {
+                method: "PUT"
+            });
+
+            if (response.status === 200) {
+                const cloneUsers = [...listaUsuarios]
+                const newUsers = cloneUsers.map(user => {
+                    if (user.id === id) {
+                        return {
+                            ...user,
+                            autorizado: !user.autorizado
+                        }
+                    }
+                    return user
+                })
+                setListaUsuarios(newUsers)
+                message.success(`Acceso actualizado`)
+            }else{
+                message.error("Error al actualizar el acceso")
+            }
+        } catch (error) {
+            console.log(error)
+            message.error("Error de conexion o de servidor al actualizar el acceso")
+        }finally{
+            hiddenMessage()
+        }
+    };
+
+    const saveClient = async (data) => {
+        const hiddenMessage = message.loading("Guardando...", 0);
+        const formData = new FormData();
+        try {
+            if (!data.userDni || !data.userName) {
+                throw new Error("No es posible crear el cliente, faltan datos!")
+            }
+            for (const key in data) {
+                formData.append(key, data[key]);
+            }
+            const response = await fetch(`${baseUrl.api}/save-client`, {
+                method: "POST",
+                body: formData
+            });
+            if (!response.ok) {
+                throw new Error("Error al crear el cliente");
+            }
+    
+            const responseData = await response.json();
+            await getAllClients()
+            notification.success({
+                message: "Se guardó el nuevo cliente",
+                description: responseData.message,
+                placement: "topRight",
+                duration: 5
+            })
+        } catch (error) {
+            console.log(error)
+            notification.error({
+                message: "Error al crear el cliente",
+                description: "Error de conexión o de servidor, verifique su conexión e intente nuevamente",
+                placement: "topRight",
+                duration: 5
+            })
+        } finally {
+            hiddenMessage();
+        }
+    };
+    const [clients, setClients] = useState([])
+    const getAllClients = async()=> {
+        const hiddenMessage = message.loading("Cargando...", 0);
+        try {
+            const response = await fetch(`${baseUrl.api}/get-all-clients`);
+            if (!response.ok) {
+                throw new Error();
+            }
+
+            const responseData = await response.json();
+            setClients(responseData.clientes)
+        } catch (error) {
+            console.log(error)
+            notification.error({
+                message: "Error al obtener los nuevos clientes",
+                description: "Error de conexión o de servidor, verifique su conexión e intente nuevamente",
+                placement: "topRight",
+                duration: 5
+            })
         }finally{
             hiddenMessage()
         }
     }
 
+    const editClient = async(clientValues, clientId) => {
+        const hiddenMessage = message.loading("Actualizando cliente...",0)
+        const formData = new FormData();
+        if (!clientId) {
+            notification.error({
+                message: "Error al actualizar el cliente",
+                description: "Faltan datos obligatorios!",
+                placement: "topRight",
+                duration: 5
+            })
+            return;
+        }
+        for (const key of Object.keys(clientValues)) {
+            formData.append(key, clientValues[key])
+        }
+
+        try {
+            const response = await fetch(`${baseUrl.api}/edit-client/${clientId}`,{
+                method: "PUT",
+                body: formData
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                console.log(data)
+                throw new Error(`${data.message}`);
+            }else{
+                await getAllClients()
+                notification.success({
+                    message: "Se actualizo el cliente",
+                    description: "Se actualizo correctamente el cliente!",
+                    placement: "topRight",
+                    duration: 5
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            notification.error({
+                message: "Error al actualizar el cliente",
+                description: error.message,
+                placement: "topRight",
+                duration: 5
+            })
+        }finally{
+            hiddenMessage()
+        }
+    } 
+    
+
     useEffect(() => {
         if (user) {
-            (async()=>{
+            (async () => {
                 const hiddenMessage = message.loading("Verificando usuario...", 0);
                 await verifyUser();
                 hiddenMessage()
@@ -138,23 +286,24 @@ export const AppContextProvider = ({ children }) => {
         }
     }, [user]);
 
-    useEffect(()=>{
+    useEffect(() => {
         if (administrator) {
             getAllAllowUsers()
         }
-    },[administrator])
+    }, [administrator])
 
-    useEffect(()=>{
+    useEffect(() => {
         if (listaUsuarios && listaUsuarios.length > 0) {
-            setCurrentUser(listaUsuarios.find((us) => 
+            setCurrentUser(listaUsuarios.find((us) =>
                 us.userid === user.id
             ))
         }
-    },[listaUsuarios])
+    }, [listaUsuarios])
     return (
         <AppContext.Provider value={{
-            administrator,authorized, listaUsuarios, nonAuthorized,
-            deleteUser,currentUser
+            administrator, authorized, listaUsuarios, nonAuthorized,
+            deleteUser, currentUser,grantOrDenyAccess, saveClient,
+            clients,editClient
         }}
         >
             {contextHolder}
