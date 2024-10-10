4e4e4a390f3b0d52daa5d6efa3bda8162465f12b
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-10 16:30:27 -0300

    Se ocultan o se muestran links en funcion de los permisos

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,38 +1,57 @@
-import React, { useEffect } from 'react'
-import "./navbar.css"
-import { SignOutButton } from '@clerk/clerk-react'
-import Link from 'antd/es/typography/Link'
-import { useNavigate } from 'react-router-dom'
-import { useAppContext } from '../../context/AppContext'
-import { Space } from 'antd'
-function Navbar() {
-    const navigate = useNavigate()
-    const { currentUser } = useAppContext()
+import React from 'react';
+import "./navbar.css";
+import { SignOutButton } from '@clerk/clerk-react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAppContext } from '../../context/AppContext';
+import { Space } from 'antd';
 
-  return (
-    <>
+function Navbar() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { currentUser } = useAppContext();
+
+    return (
         <header className='navbar'>
             <nav className='nav-pc'>
                 <div className="user__info">
                     <picture className="user__image">
                         <img src={currentUser.userimage} alt="" />
                     </picture>
-                    <div className="user__name"><Space> {currentUser.username} <SignOutButton className="logout__button" /></Space></div>
+                    <div className="user__name">
+                        <Space> 
+                            {currentUser.username} 
+                            <SignOutButton className="logout__button" />
+                        </Space>
+                    </div>
                 </div>
                 <ul className='nav-pc__links'>
-                    <li className='nav-pc__li'>Inicio</li>
-                    <li className='nav-pc__li' onClick={()=> navigate("/clientes")}>Clientes</li>
+                    {currentUser?.administrador && (
+                        <li 
+                            className={location.pathname === "/dashboard" ? "nav-pc__li active" : "nav-pc__li"} 
+                            onClick={() => navigate("/dashboard")}
+                        >
+                            Inicio
+                        </li>
+                    )}
+                    <li 
+                        className={location.pathname === "/clientes" ? 'nav-pc__li active' : 'nav-pc__li'} 
+                        onClick={() => navigate("/clientes")}
+                    >
+                        Clientes
+                    </li>
                     <li className='nav-pc__li'>Cuentas C.</li>
                     <li className='nav-pc__li'>Pagos</li>
                     <li className='nav-pc__li'>Vencimientos</li>
-                    <li className='nav-pc__li'>Reportes</li>
-                    <li className='nav-pc__li'>Ajustes</li>
-                    {/* <a href="#">{"Cerrar Sesión"}</a> */}
+                    {currentUser?.administrador && (
+                        <li className='nav-pc__li'>Reportes</li>
+                    )}
+                    {currentUser?.administrador && (
+                        <li className='nav-pc__li'>Ajustes</li>
+                    )}
                 </ul>
             </nav>
         </header>
-    </>
-  )
+    );
 }
 
-export default Navbar
+export default Navbar;
