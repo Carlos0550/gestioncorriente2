8310797879887d95ec4b7a31fb4229ed98283967
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-12 01:22:57 -0300

    Envuelve ConfigProvider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import { AppContextProvider } from './context/AppContext';
 import {BrowserRouter} from "react-router-dom"
 import { ClerkProvider } from '@clerk/clerk-react'
+import { ConfigProvider } from 'antd';
+import es_ES from "antd/locale/es_ES"
+
 const publishableKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +14,9 @@ root.render(
     <ClerkProvider publishableKey={publishableKey} afterSignOutUrl="/">
       <BrowserRouter>
         <AppContextProvider>
+        <ConfigProvider locale={es_ES}>
           <App />
+          </ConfigProvider>
         </AppContextProvider>
       </BrowserRouter>
     </ClerkProvider>
