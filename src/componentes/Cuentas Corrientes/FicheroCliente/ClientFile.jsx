b6a336af96e4b178152f3bbce0415df713c6b3b0
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-12 01:21:46 -0300

    Refactorizacion
    Cambios en la estructura del código

--- src/componentes/Cuentas Corrientes/FicheroCliente/ClientFile.jsx
+++ src/componentes/Cuentas Corrientes/FicheroCliente/ClientFile.jsx
@@ -1,87 +1,161 @@
-import { Card, Col, Collapse, message, notification, Row } from 'antd';
+import { Button, Card, Col, Collapse, notification, Row, Skeleton, Table } from 'antd';
 import React, { useEffect, useRef, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { baseUrl } from '../../../config';
-import AddDebt from './AddDebt';
+import AddDebt from './FormProductos/AddDebt';
+import { useAppContext } from '../../../context/AppContext';
+import dayjs from 'dayjs';
 
+import "./css/clientFile.css"
+import { SettingFilled } from '@ant-design/icons';
+import AddDelivers from './FormEntregas/AddDelivers';
 function ClientFile() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const clientId = queryParams.get("clientId");
+    const { getClientFile, client, processedDebts, capitaliceStrings } = useAppContext()
 
-    const [client, setClient] = useState([]);
-    const [errorFetching, setErrorFetching] = useState(false)
+    const alreadyVerified = useRef(false)
+    const [findingFile, setFindingFile] = useState(true)
     const navigate = useNavigate();
-    if (!clientId) {
-        notification.error({
-            message: "Error al obtener el cliente",
-            duration: 3,
-            showProgress: true
-        })
-        setTimeout(() => {
-            navigate("/cuentas-corrientes")
-        }, 1000 * 3);
-    }
-
+    useEffect(() => {
+        if (!clientId && !alreadyVerified.current) {
+            alreadyVerified.current = true
+            notification.error({
+                message: "Error al obtener el cliente",
+                duration: 3,
+                showProgress: true
+            })
+            setTimeout(() => {
+                navigate("/cuentas-corrientes")
+            }, 1000);
+        }
+    }, [clientId])
 
     const alreadyFetch = useRef(false)
-    useEffect(()=>{
+    useEffect(() => {
         if (clientId && !alreadyFetch.current) {
-            (async()=>{
+            (async () => {
                 alreadyFetch.current = true
-
-                const hiddenMessage = message.loading("Un momento...",0)
-                try {
-                    const response = await fetch(`${baseUrl.api}/get-client-file/${clientId}`);
-                    if (!response.ok) {
-                        throw new Error("Error obteniendo los datos del cliente");
-                    }
-                    const data = await response.json()
-                    setClient(data)
-                } catch (error) {
-                    console.log(error)
-                    notification.error({
-                        message: "Error al obtener el cliente",
-                        description: error.message,
-                        placement: "topRight",
-                        duration: 3,
-                        showProgress: true
-                    })
-                    navigate("/cuentas-corrientes")
-                }finally{
-                    hiddenMessage()
-                }
+                setFindingFile(true)
+                await getClientFile(clientId)
+                setTimeout(() => {
+                    setFindingFile(false)
+                }, 1000);
             })()
         }
-    },[clientId])
-    // useEffect(()=>{
-    //     console.log(client)
-    // },[client])
+    }, [clientId])
 
+    const getTotal = () => {
+        let total = 0
+        processedDebts.forEach(debt => {
+            debt.productos.forEach(prod => {
+                total += prod.precio * prod.cantidad
+            });
+        });
+
+        return parseFloat(total).toLocaleString("es-AR", { style: "currency", currency: "ARS" })
+    }
+    const tableColumns = [
+        {
+            key: 1,
+            title: "Fecha de compra",
+            render: (_, record) => (
+                <>
+                    {dayjs(record.fechaCompra).format("DD/MM/YYYY")}
+                </>
+            )
+        },
+        {
+            key: 2,
+            title: "Vencimiento",
+            render: (_, record) => (
+                <>
+                    {dayjs(record.fechaVencimiento).format("DD/MM/YYYY")}
+                </>
+            )
+        },
+        {
+            key: 3,
+            title: "Estado del pago",
+            render: (_, record) => (
+                <>
+                    <p style={{ color: record.estado === "Pendiente" ? "red" : "green" }}>{record.estado}</p>
+                </>
+            )
+        },
+        {
+            key: 3,
+            title: "Detalles",
+            render: (_, record) => (
+                <>
+                    <ul>
+                        {record.productos.map((prod) => (
+                            <li>{prod.cantidad} {prod.nombre} ${prod.precio}</li>
+                        ))}
+                    </ul>
+                </>
+            )
+        },
+        {
+            key: 4,
+            title: (
+                <>Saldo total: <span style={{ color: "green" }}>{getTotal()}</span></>
+            )
+        }
+    ]
     return (
         <>
-            <Row gutter={[16, 16]} style={{
-                margin: ".5rem"
-            }}>
-                <Col xs={24} md={14} lg={12}>
-                    <Card title="Fichero del cliente" style={{ minWidth: "100%" }}>
-                        <Collapse
-                        items={[
-                            {
-                                key: 1,
-                                label: "Agregar productos",
-                                children: <AddDebt clientId={clientId}/>
-                            }
-                        ]}
-                        />
-                    </Card>
-                </Col>
-                <Col xs={24} md={10} lg={12}>
-                    <Card title="Entregas de dinero" style={{ minWidth: "100%" }}>
+            <div className="file__wrapper">
+                <h1 className='file__client-name'>{capitaliceStrings(client?.nombre_cliente) ?? ""} <Button icon={<SettingFilled />}></Button></h1>
 
-                    </Card>
-                </Col>
-            </Row>
+                <Row gutter={[16, 16]} style={{
+                    margin: ".5rem"
+                }}>
+                    <Col xs={24} md={14} lg={12}>
+
+
+                        <Card title={`Fichero de: ${capitaliceStrings(client?.nombre_cliente)?.split(" ")[0] ?? ""}`} style={{ minWidth: "100%" }} hoverable bordered={false}>
+                            {findingFile ? <Skeleton active /> : (
+                                <>
+                                    <Collapse
+                                        items={[
+                                            {
+                                                key: 1,
+                                                label: "Agregar productos",
+                                                children: <AddDebt clientId={clientId} />
+                                            }
+                                        ]}
+                                    />
+                                    <Table
+                                        columns={tableColumns}
+                                        dataSource={processedDebts}
+                                        pagination={false}
+                                        scroll={{ x: 500 }}
+                                        rowKey={record => record.id}
+                                    />
+                                </>
+                            )}
+                        </Card>
+                    </Col>
+                    <Col xs={24} md={10} lg={12}>
+                        <Card title="Entregas de dinero" style={{ minWidth: "100%" }} hoverable bordered={false}>
+                            {findingFile ? <Skeleton active /> : (
+                                <>
+                                    <Collapse
+                                        items={[
+                                            {
+                                                key: 1,
+                                                label: "Agregar entregas",
+                                                children: <AddDelivers clientId={clientId} />
+                                            }
+                                        ]}
+                                    />
+                                </>
+                            )}
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         </>
     )
 }
