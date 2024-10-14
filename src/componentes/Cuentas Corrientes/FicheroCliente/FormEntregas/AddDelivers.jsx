c78d0472f2fe4623432ada21596ea8e39182ec90
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-14 07:53:26 -0300

    Adaptabilidad para editar entregas

--- src/componentes/Cuentas Corrientes/FicheroCliente/FormEntregas/AddDelivers.jsx
+++ src/componentes/Cuentas Corrientes/FicheroCliente/FormEntregas/AddDelivers.jsx
@@ -1,17 +1,43 @@
 import { Button, DatePicker, Form, Input } from 'antd'
 import dayjs from 'dayjs'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../../../../context/AppContext'
 
-function AddDelivers({clientId}) {
+function AddDelivers({clientId, editingDeliver = false, selectedDeliver = {}, closeModal, totalDebts}) {
+
     const [form] = Form.useForm()
-    const [deliverDate, setDeliverDate] = useState(dayjs())
-    const onFinish = (values) => {
-        console.log('Success:', values);
+    const [deliverDate, setDeliverDate] = useState(dayjs().format("YYYY-MM-DD"))
+    const { saveClientDeliver, editDeliver } = useAppContext()
+    const [savingDeliver, setSavingDeliver] = useState(false)
+    const onFinish = async(values) => {
+        setSavingDeliver(true)
+        const { deliverAmount, deliverDate } = values
+        const deliver = {
+            deliverAmount,
+            deliverDate: dayjs(deliverDate).format("YYYY-MM-DD")
+        }
+        editingDeliver ? await editDeliver(deliver,clientId, selectedDeliver.id) : await saveClientDeliver(deliver,clientId)
+        setSavingDeliver(false)
+        form.resetFields()
+        if (editingDeliver) {
+            closeModal()
+        }
     }
 
     const handleDateChange = (date) =>{
         setDeliverDate(date)
     }
+
+    useEffect(()=>{
+        if (editingDeliver && selectedDeliver) {
+            form.setFieldsValue({
+                deliverAmount: selectedDeliver.monto,
+                deliverDate: dayjs(selectedDeliver.fecha)
+            })
+
+            setDeliverDate(dayjs(selectedDeliver.fecha))
+        }
+    },[editingDeliver, selectedDeliver])
   return (
     <Form
     name='deliversForm'
@@ -20,7 +46,7 @@ function AddDelivers({clientId}) {
     >
         <Form.Item
         name={"deliverAmount"}
-        label="Monto a entregar"
+        label={editingDeliver ? "Ingrese una entrega" : `Ingrese una entrega no mayor a ${parseFloat(totalDebts).toLocaleString("es-AR",{style:"currency",currency:"ARS"})}`}
         rules={[
             {validator:(_,value) => {
                 const regex = /^[0-9]+$/
@@ -28,6 +54,8 @@ function AddDelivers({clientId}) {
                     return Promise.reject("Por favor, ingrese un monto válido mayor a 0")
                 }else if(!regex.test(value)){
                     return Promise.reject("Por favor, ingrese un monto válido")
+                }else if(!editingDeliver && value > totalDebts){
+                    return Promise.reject(`Por favor, ingrese un monto menor a ${parseFloat(totalDebts).toLocaleString("es-AR",{style:"currency",currency:"ARS"})}`)
                 }
                 return Promise.resolve()
             }}
@@ -45,7 +73,7 @@ function AddDelivers({clientId}) {
             <DatePicker value={deliverDate} onChange={(date)=>handleDateChange(date)}/>
         </Form.Item>
         <Form.Item>
-            <Button type='primary' htmlType='submit'>Guardar entrega</Button>
+            <Button type='primary' htmlType='submit' loading={savingDeliver}>Guardar entrega</Button>
         </Form.Item>
     </Form>
   )
