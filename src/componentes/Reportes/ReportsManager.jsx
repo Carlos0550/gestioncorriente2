36dcfe2aabee241bf2b119153719a022a43ec04f
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-19 08:17:49 -0300

    Bug: solucionando problemas con los logs

--- src/componentes/Reportes/ReportsManager.jsx
+++ src/componentes/Reportes/ReportsManager.jsx
@@ -1,8 +1,9 @@
-import { notification, Space, Table } from 'antd';
+import { notification, Space, Table, Typography } from 'antd';
 import React, { useEffect, useRef, useState } from 'react';
 import Navbar from '../Navbar/Navbar';
 import { baseUrl } from '../../config';
 import dayjs from "dayjs"
+const { Text } = Typography
 function ReportsManager() {
   const [reportes, setReportes] = useState([]);
 
@@ -32,56 +33,15 @@ function ReportsManager() {
       })();
     }
   }, []);
+  console.log("Reportes: ", reportes)
 
-  const actionTypes = {
-    insert: "Crear",
-    create: "Crear",
-    update: "Actualizar",
-    delete: "Eliminar",
-
-  };
-
-  const transformedReports = reportes.map(report => ({
-    ...report,
-    detalles: `${report.details} el día ${dayjs(report.day).format("DD/MM/YYYY")} a las ${report.time}`,
-    action_type: actionTypes[report.action_type] ? actionTypes[report.action_type] : report.action_type,
-  }));
-
-  const columns = [
-    {
-      title: 'Usuario',
-      render: (_, record) => (
-        <Space>
-          <p>{record.user_name}</p>
-          <picture className="user__image-container"><img src={record.user_image} alt="" /></picture>
-        </Space>
-      ),
-      key: 'user_name',
-    },
-    {
-      title: 'Tipo de Acción',
-      dataIndex: 'action_type',
-      key: 'action_type',
-    },
-    {
-      title: 'Entidad',
-      dataIndex: 'entity',
-      key: 'entity',
-    },
-    {
-      title: 'Detalles',
-      dataIndex: 'detalles',
-      key: 'detalles',
-    },
-  ];
-
+ 
 
   return (
     <>
       <Navbar />
       <h1 className='title'>Reportes</h1>
       <div className="container-wrapper">
-        <Table dataSource={transformedReports} columns={columns} rowKey="id" scroll={{ x: 500 }} />
       </div>
     </>
   );
