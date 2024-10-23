012ca99c48f12008fff9b055c1b3a5f61ce6c29a
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-23 19:13:10 -0300

    Actualizacion
    se movio el formulario a un modulo sepadado y se hicieron cambios visuales

--- src/componentes/Ajustes/SettingsManager.jsx
+++ src/componentes/Ajustes/SettingsManager.jsx
@@ -1,21 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from '../Navbar/Navbar'
-import { Card, Form, Table, Input, Row, Col, Collapse, Button, message } from 'antd'
+import { Card, Form, Table, Input, Row, Col, Collapse, Button, message, notification, Space } from 'antd'
 import { useAppContext } from '../../context/AppContext';
+import AddBranches from './Añadir Sucursales/AddBranches';
+import { baseUrl } from '../../config';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 const { Panel } = Collapse;
 
 function SettingsManager() {
-    const [form] = Form.useForm();
     const [activeKey, setActiveKey] = useState(null);
-    const { saveBranch } = useAppContext()
+    const [sucursales, setSucursales] = useState([])
 
-    const onFinish = async(values) => {
-        const { businessName } = values
-        if (!businessName) return message.error("Se necesita un nombre de sucursal/negocio!")
-        await saveBranch(businessName)
-        form.resetFields()
-        
+    const alreadyFetch = useRef(false)
+    useEffect(() => {
+        if (!alreadyFetch.current) {
+            (async () => {
+                alreadyFetch.current = true
+
+                try {
+                    const response = await fetch(`${baseUrl.api}/get-branches`)
+                    const data = await response.json()
+                    console.log(data)
+                    if(response.status !== 200) throw new Error("Error al obtener las sucursales")
+                    setSucursales(data.sucursales)
+                } catch (error) {
+                    console.log(error)
+                    notification.error({
+                        message: error.message,
+                        duration: 3,
+                        showProgress: true
+                    })
+                }
+            })()
+        }
+    }, [])
+
+    const [selectedBranch, setSelectedBranch] = useState({})
+    const [isEditingBranch, setIsEditingBranch] = useState(false)
+    const handleEditBranch = (branchId) => {
+        setSelectedBranch(sucursales.find(sucursal => sucursal.id === branchId))
+        setIsEditingBranch(true)
+    }
+
+    const handleCancelEditBranch = () =>{
+        setIsEditingBranch(false)
+        setSelectedBranch({})
     }
 
     const settingTable = [
@@ -23,7 +53,18 @@ function SettingsManager() {
             key: "1",
             title: "Puntos de venta",
             render: (_, record) => (
-                <></>
+                <>{record.business_name}</>
+            )
+        },
+        {
+            key: "2",
+            render: (_, record) => (
+                <>
+                    <Space direction='vertical'>
+                        <Button icon={<EditOutlined/>} type='primary' onClick={()=> handleEditBranch(record.id)}></Button>
+                        <Button icon={<DeleteOutlined/>} type='primary' danger></Button>
+                    </Space>
+                </>
             )
         }
     ];
@@ -38,37 +79,21 @@ function SettingsManager() {
                         activeKey={activeKey}
                         onChange={(key) => setActiveKey(key.length > 0 ? key : null)}
                     >
-                        <Panel header="Sucursales" key="1">
-                            <Row gutter={[16, 16]}>
-                                <Col xs={24} sm={24} md={12} lg={12}>
+                        <Panel header="Sucursales" key="1" >
+                            <Row gutter={[16, 16]} >
+                                <Col xs={24} sm={24} md={12} lg={12} style={{ padding: 0 }}>
                                     <Table
                                         columns={settingTable}
-                                        pagination={false}
+                                        dataSource={sucursales}
+                                        pagination={{pageSize: 5}}
                                         scroll={{ x: '100%' }}
+                                        
                                     />
                                 </Col>
 
-                                <Col xs={24} sm={24} md={12} lg={12}>
-                                <h3>Agregar sucursal</h3>
-                                    <Form
-                                        form={form}
-                                        layout='vertical'
-                                        onFinish={onFinish}
-                                    >
-                                        <Form.Item
-                                            name={"businessName"}
-                                            label="Nombre del local/negocio"
-                                            rules={[
-                                                { type: "string", required: true, message: "El nombre del local/negocio es obligatorio" }
-                                            ]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                        <Form.Item
-                                        >
-                                            <Button htmlType='submit' type='primary'>Guardar sucursal</Button>
-                                        </Form.Item>
-                                    </Form>
+                                <Col xs={24} sm={24} md={12} lg={12} style={{ padding: 0 }}>
+                                    <h3>Agregar sucursal</h3> {isEditingBranch ? <Button onClick={()=> handleCancelEditBranch()} type='primary' danger>Cancelar</Button> : null}
+                                    <AddBranches cancelEdit={()=> handleCancelEditBranch()} selectedBranch={selectedBranch} isEditing={isEditingBranch}/>
                                 </Col>
                             </Row>
                         </Panel>
